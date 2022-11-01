f55cbed1f7f250f2e8cc41966ba5f578538e5130
KenjiSeah <kenjiseah.21@ichat.sp.edu.sg>
2022-11-01 09:51:26 +0800

    Lab 2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,34 @@
-print("Hello World!")
+def display_main_menu():
+    print('Enter some numbers separated by commas (eg.5,67,32)')
+
+def get_user_input():
+   numbers = input("Enter numbers:")
+   list = numbers.split(",")
+   for i in range(0,len(list)):
+       list[i] = float(list[i])
+
+   return list
+
+
+
+def calc_average_temperature(num_list):
+    total = 0
+    for i in range (0,len(num_list)):
+        total = total + num_list[i]
+    average = total/len(num_list)
+    print (average)
+
+
+
+
+
+
+def main():
+    print("ET0735  (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    calc_average_temperature(num_list)
+
+if __name__ == "__main__":
+    main()
+
